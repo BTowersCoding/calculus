2742af98b53b0309911cf4450700fd631dff740f
porkostomus <porkostomus@mail.com>
2021-10-25 20:05:40 -0700

    init shadow-cljs

---
+++ src/calculus/app.cljs
@@ -0,0 +1,21 @@
+(ns calculus.app
+  (:require [reagent.core :as r]))
+
+(defn app []
+  [:div#app
+   [:h1 "Calculus module"]])
+
+(defn render []
+  (r/render [app]
+            (.getElementById js/document "root")))
+
+(defn ^:dev/after-load start []
+  (render)
+  (js/console.log "start"))
+
+(defn ^:export init []
+  (js/console.log "init")
+  (start))
+
+(defn ^:dev/before-load stop []
+  (js/console.log "stop"))
